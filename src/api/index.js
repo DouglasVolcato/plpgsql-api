58d08aaa2b8d5c448bf781c70f187fa7f4fcfc98
DouglasVolcato <douglasvolcato@gmail.com>
2024-03-29 01:56:53 -0300

    feat: add dinamic routes from database

--- src/api/index.js
+++ src/api/index.js
@@ -2,17 +2,46 @@ import bodyParser from "body-parser";
 import { config } from "dotenv";
 import express from "express";
 import cors from "cors";
-config();
+import pg from "pg";
+const { Pool } = pg;
 
+config();
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-app.get("/", (req, res) => {
-  res.send("Working");
+const pool = new Pool({
+  host: "postgres",
+  user: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
+  port: Number(process.env.POSTGRES_PORT),
 });
 
-const port = process.env.API_PORT || 3000;
-app.listen(port, () => {
-  console.log(`http://localhost:${port}`);
+const client = await pool.connect();
+const result = await client.query("SELECT * FROM router()");
+
+await Promise.all(
+  result.rows.map(async (route) => {
+    app[route.type](route.path, async (req, res) => {
+      try {
+        const data = JSON.stringify({
+          ...req.body,
+          ...req.query,
+          ...req.params,
+        });
+        const result = await client.query(
+          `SELECT * FROM ${route.function}('${data}')`
+        );
+        res.status(200).send(result);
+      } catch (error) {
+        res.status(500).send(error);
+      }
+    });
+  })
+).then(() => {
+  const port = process.env.PORT || 3000;
+  app.listen(port, () => {
+    console.log(`http://localhost:${port}`);
+  });
 });
