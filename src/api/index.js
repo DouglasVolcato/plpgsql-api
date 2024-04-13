9a773156c618c72934059b04b884dc019d946f29
DouglasVolcato <douglasvolcato@gmail.com>
2024-04-13 18:04:00 -0300

    refactor: add postgres host variable

--- src/api/index.js
+++ src/api/index.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 const pool = new Pool({
-  host: "postgres",
+  host: process.env.POSTGRES_HOST,
   user: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DB,
