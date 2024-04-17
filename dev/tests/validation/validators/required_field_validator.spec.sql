8be195e2861a3471e6378f713c624ef6555de3b4
DouglasVolcato <douglasvolcato@gmail.com>
2024-04-16 21:33:18 -0300

    chore: ensure required_field_validator is working

---
+++ dev/tests/validation/validators/required_field_validator.spec.sql
@@ -0,0 +1,19 @@
+CREATE OR REPLACE FUNCTION required_field_validator_spec() RETURNS BOOLEAN AS $$
+DECLARE
+    result BOOLEAN;
+BEGIN
+    SELECT required_field_validator('{"valid_property": "value"}'::JSONB, 'valid_property') INTO result;
+        IF NOT result THEN
+            RAISE EXCEPTION 'required_field_validator should return true if property exists';
+        END IF;
+
+    SELECT required_field_validator('{"invalid_property": "value"}'::JSONB, 'valid_property') INTO result;
+        IF result THEN
+            RAISE EXCEPTION 'required_field_validator should return false if property does not exist';
+        END IF;
+        
+    RETURN TRUE;
+END;
+$$ LANGUAGE plpgsql;
+
+SELECT required_field_validator_spec();
